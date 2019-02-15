14788c6cee9a58a6b38d9ce6d97165e32bcd0d69
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-02-15 14:23:12 +0100

    added contactlist with address and pictures

--- src/components/contacts/ContactsScreen.js
+++ src/components/contacts/ContactsScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Title, Container, Header, Left, Body, Right, Button, Icon, Content} from 'native-base';
-import AddContact from './AddContact';
-import ContactDetail from './ContactDetail';
+import List from './List';
+
+
 
 export default class ContactsScreen extends Component {
 
@@ -10,28 +11,25 @@ export default class ContactsScreen extends Component {
         const title = {text: 'Kontakte'};
 
 
-        return (
-            <Container>
-                <Header>
-                    <Left>
-                        <Button transparent>
-                            <Icon name='arrow-back'/>
-                        </Button>
-                    </Left>
-                    <Body>
-                    <Title>{title.text}</Title>
-                    </Body>
-                    <Right/>
-                </Header>
-                <Content>
-                <ContactDetail />
-                <AddContact />
+        return <Container style={{flex:1}}>
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name='arrow-back'/>
+                    </Button>
+                </Left>
+                <Body>
+                <Title>{title.text}</Title>
+                </Body>
+                <Right/>
+            </Header>
+            <Content>
 
-                </Content>
+               <List />
 
-            </Container>
+            </Content>
 
-        );
+        </Container>;
     }
 
 }
---
+++ src/components/contacts/List.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import {Container, Content, Card, CardItem, Grid, Col, Text, Row, Thumbnail} from 'native-base';
+import {StyleSheet, View} from "react-native";
+import datas from '../../mocks/contacts_data';
+
+
+class List extends Component {
+
+
+    getList = () => {
+
+        const listItems = datas.map((contact) => {
+            return (
+                <Card key={contact.contactId}>
+                    <CardItem>
+                        <Grid style={styles.grid}>
+                            <Col style={styles.imageCol}>
+                                {contact.uri &&
+                                <Thumbnail source={{uri: contact.uri}}/>
+                                }
+                            </Col>
+                            <Col>
+                                <Row>
+                                    <Text style={styles.name}> {contact.first} {contact.last}</Text>
+                                </Row>
+                                <Row>
+                                    <Text style={styles.address}> {contact.street} {contact.number}</Text>
+                                </Row>
+                                <Row>
+                                    <Text style={styles.address}> {contact.zipCode} {contact.city}</Text>
+                                </Row>
+                                <Row>
+                                    <Text style={styles.tel}> {contact.phone} </Text>
+                                </Row>
+                            </Col>
+                        </Grid>
+
+
+                    </CardItem>
+                </Card>
+            );
+        });
+
+        return (
+            <View>
+                {listItems}
+            </View>);
+
+    };
+
+
+    render() {
+
+        let list = this.getList();
+
+        return (
+
+            <Container>
+                <Content>
+                    {list}
+
+                </Content>
+            </Container>
+
+
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 70,
+        height: 70,
+        marginRight: 5
+    },
+    grid: {
+        marginTop: 10,
+        marginBottom: 10
+    },
+    imageCol: {
+        width: 80
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    address: {
+        fontSize: 16
+    },
+    tel: {
+        fontSize: 14
+    }
+});
+
+export default List;
