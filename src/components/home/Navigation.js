3b40f57283ce2df2445b542619148597798d36f9
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-02-19 11:26:34 +0100

    styled navigationbar

--- src/components/home/Navigation.js
+++ src/components/home/Navigation.js
@@ -2,56 +2,48 @@ import React from 'react';
 import ContactsScreen from '../contacts/ContactsScreen';
 import SettingsScreen from '../settings/SettingsScreen';
 import HomeScreen from './HomeScreen';
-import { createBottomTabNavigator,createAppContainer, createStackNavigator } from 'react-navigation';
-import ContactItemScreen from '../contacts/ContactItemScreen';
-
+import {createBottomTabNavigator, createAppContainer, createStackNavigator} from 'react-navigation';
+import ContactDetail from '../contacts/ContactDetail';
 
 const TabScreens = createBottomTabNavigator({
     Home: {
         screen: HomeScreen,
-        tabBarOptions: {
-            title: 'Contacts'}
     },
     Contacts: {
         screen: ContactsScreen,
-        tabBarOptions: {
-            title: 'Contacts',
-            activeTintColor: '#e91e63',
-            labelStyle: {
-                fontSize: 12,
-            },
-            style: {
-                backgroundColor: '#e91e63',
-                margin: 5,
-                paddingBottom: 10
-            },
-        },
+
         tabBarPosition: 'top'
     },
     Settings: {
         screen: SettingsScreen,
-        tabBarOptions: {
-            title: 'Contacts',
-            activeTintColor: '#696969',
-            labelStyle: {
-                fontSize: 12,
-            },
-            style: {
-                backgroundColor: '#e91e63',
-                margin: 5,
-                paddingBottom: 10
-            },
+
+    }
+}, {
+
+    tabBarOptions: {
+        activeTintColor: '#262626',
+        inactiveTintColor: '#FFFFFF',
+        labelStyle: {
+            fontSize: 18,
+            fontWeight: '400'
+
         },
-        tabBarPosition: 'top'
+        style: {
+            backgroundColor: '#e91e63',
+            margin: 20,
+            paddingBottom: 15
         }
-    });
+    }
+});
+
 
 const AppNavigator = createStackNavigator({
     Root: {
-        screen: TabScreens
+        screen: TabScreens,
+
     },
-    Item: {
-        screen: ContactItemScreen
+    Details: {
+        screen: ContactDetail
     }
 
 });
