622c71b7c45cdf8e338b09ac4f14ccf126151370
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-02-28 13:32:54 +0100

    add google map view in a component

---
+++ src/components/googleView/GoogleMapView.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {StyleSheet, View} from "react-native";
+import MapView from 'react-native-maps';
+import {Marker} from 'react-native-maps';
+
+
+class GoogleMapView extends Component {
+
+    render() {
+
+
+        return (
+            <View
+                style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 49.694671,
+                        longitude: 8.574482,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+    }
+});
+
+
+export default GoogleMapView;
