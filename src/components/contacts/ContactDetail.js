5e31f7e9d27d8e0c5bffea8f278857874c9f853f
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-04-02 15:03:24 +0200

    update contact detail screen

--- src/components/contacts/ContactDetail.js
+++ src/components/contacts/ContactDetail.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
-import {Container, Content, Text, Thumbnail, Grid, Col, Icon, Header, Left, Body, Title, Right, Button} from 'native-base';
-import {StyleSheet } from 'react-native';
-import GoogleMapView from '../googleView/GoogleMapView';
+import {
+    Container,
+    Content,
+    Text,
+    Thumbnail,
+    Grid,
+    Col,
+    Icon,
+    Header,
+    Left,
+    Body,
+    Title,
+    Right,
+    Button
+} from 'native-base';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import GoogleMapView from "../googleView/GoogleMapView";
+import CoordinatesContainer from "../../containers/CoordinatesContainer";
 
 
 class ContactDetail extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            getCurrentLocationButtonClicked: false
+
+        }
+    }
+
+    getPosition = () => {
+
+        this.setState({
+            getCurrentLocationButtonClicked: true
+        })
+    }
+
     render() {
-        console.log('props: ', this.props.navigation.getParam('contact'));
+        //console.log('props: ', this.props.navigation.getParam('contact'));
+        console.log('render contactDetails');
         const {
             first,
             last,
@@ -20,41 +51,49 @@ class ContactDetail extends Component {
 
         return (
 
-                <Container style={styles.container}>
-                    <Content style={styles.text}>
-                        <Header>
-                            <Left>
-                                <Button
-                                    transparent
-                                    onPress={() => this.props.navigation.goBack()}
-                                >
-                                    <Icon name='arrow-back'/>
-                                </Button>
-                            </Left>
-                            <Body>
-                            <Title>{title.text}</Title>
-                            </Body>
-                            <Right/>
-                        </Header>
-                        <Grid style={styles.grid}>
-                            <Col style={styles.imageCol}>
-                                <Thumbnail large style={styles.image} source={{uri: image}}/>
-                            </Col>
-                            <Col>
-                                <Text style={styles.title}> Name: </Text>
-                                <Text style={styles.field}> {first} {last} </Text>
-                                <Text style={styles.title}> Adresse: </Text>
-                                <Text style={styles.field}> {addresses[0].street} {addresses[0].number}</Text>
-                                <Text style={styles.field}> {addresses[0].zipCode} {addresses[0].city}</Text>
-                                <Text style={styles.title}> Telefonnummer: </Text>
-                                <Text style={styles.field}> {phone}</Text>
-                            </Col>
-                        </Grid>
-                    </Content>
+            <Container style={styles.container}>
+                <Content style={styles.text}>
+                    <Header>
+                        <Left>
+                            <Button
+                                transparent
+                                onPress={() => this.props.navigation.goBack()}
+                            >
+                                <Icon name='arrow-back'/>
+                            </Button>
+                        </Left>
+                        <Body>
+                        <Title>{title.text}</Title>
+                        </Body>
+                        <Right/>
+                    </Header>
+                    <Grid style={styles.grid}>
+                        <Col style={styles.imageCol}>
+                            <Thumbnail large style={styles.image} source={{uri: image}}/>
+                        </Col>
+                        <Col>
+                            <Text style={styles.title}> Name: </Text>
+                            <Text style={styles.field}> {first} {last} </Text>
+                            <Text style={styles.title}> Adresse: </Text>
+                            <Text style={styles.field}> {addresses[0].street} {addresses[0].number}</Text>
+                            <Text style={styles.field}> {addresses[0].zipCode} {addresses[0].city}</Text>
+                            <Text style={styles.title}> Telefonnummer: </Text>
+                            <Text style={styles.field}> {phone}</Text>
+                        </Col>
+                    </Grid>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={
+                            this.getPosition
+                        }>
+                        <Text>open map</Text>
+                    </TouchableOpacity>
+                </Content>
 
-                    <GoogleMapView/>
+                {(this.state.getCurrentLocationButtonClicked === true ? (
+                    <CoordinatesContainer Layout={GoogleMapView}/>) : null)}
 
-                </Container>
+            </Container>
 
         );
     }
@@ -74,7 +113,6 @@ const styles = StyleSheet.create({
         color: '#4B4B4B'
     },
     title: {
-
         fontSize: 24,
         fontWeight: 'bold',
         color: '#4B4B4B'
@@ -95,6 +133,13 @@ const styles = StyleSheet.create({
     },
     imageCol: {
         width: 80
+    },
+    button: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        backgroundColor: '#E20074',
+        alignItems: 'center',
+        color: '#fff'
     }
 });
 
